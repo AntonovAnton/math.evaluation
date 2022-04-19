a9d88943d683d3b5825ffb572934c3bc6dcbf126
Anton Antonov <anton.antonov@plarium.com>
2022-04-19 23:33:59 +0300

    Evaluation math expression with Roslyn is added

--- EvaluateMathExpression/Program.cs
+++ EvaluateMathExpression/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+
+try
+{
+    const string expression = "(2 / (2 + 3.33) * 4) - -6";
+
+    var result = await CalcUsingRoslyn(expression);
+    Console.WriteLine($"Using Roslyn: {expression} = {result}");
+    return 0;
+}
+catch (Exception exception)
+{
+    Console.WriteLine(exception.ToString());
+    return 1;
+}
+
+
+async Task<double> CalcUsingRoslyn(string expression)
+{
+    try
+    {
+        var result = await CSharpScript.EvaluateAsync<double>(expression);
+        return result;
+    }
+    catch (Exception exception)
+    {
+        Console.WriteLine($"An exception has occurred while evaluating mathematical expression: {expression}");
+        Console.WriteLine(exception.ToString());
+        return double.NaN;
+    }
+}
