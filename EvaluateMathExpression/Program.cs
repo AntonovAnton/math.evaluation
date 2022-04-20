07431112a06c74be7068b24475d31842b671331c
Anton Antonov <anton.antonov@plarium.com>
2022-04-20 09:44:30 +0300

    Evaluation with recursive is implemented

--- EvaluateMathExpression/Program.cs
+++ EvaluateMathExpression/Program.cs
@@ -3,17 +3,45 @@ using Microsoft.CodeAnalysis.CSharp.Scripting;
 
 try
 {
-    //const string expression = "(2 / (2 + 3.33) * 4) - -6";
-
-    const string expression = "4 * 0.1 - 2";
-
-    var result = Calc2(expression);
+    var expression = "-(2 / (2 + 3.33) * 4) - -6";
+    var result = Calc(expression);
     Console.WriteLine($"Using recursive: {expression} = {result}");
 
+    expression = "4 * 0.1 - 2";
+    result = Calc(expression);
+    Console.WriteLine($"Using recursive: {expression} = {result}");
+
+    expression = "6 + -( -4)";
+    result = Calc(expression);
+    Console.WriteLine($"Using recursive: {expression} = {result}");
+
+    expression = "6 + -(4)";
+    result = Calc(expression);
+    Console.WriteLine($"Using recursive: {expression} = {result}");
+
+    expression = "(2.323 * 323 - 1 / (2 + 3.33) * 4) - -6";
+    result = Calc(expression);
+    Console.WriteLine($"Using recursive: {expression} = {result}");
+
+    expression = "(2.323 * 323 - 1 / -(2 + 3.33) * 4) - -6";
+    result = Calc(expression);
+    Console.WriteLine($"Using recursive: {expression} = {result}");
+
+    //the following expressions also work :)
+    expression = "1 - - 1";
+    result = Calc(expression);
+    Console.WriteLine($"Using recursive: {expression} = {result}");
+
+    expression = "6 + - (4)";
+    result = Calc(expression);
+    Console.WriteLine($"Using recursive: {expression} = {result}");
+
+    expression = "6 + -(- 4)";
+    result = Calc(expression);
+    Console.WriteLine($"Using recursive: {expression} = {result}");
 
     result = await CalcUsingRoslyn(expression);
     Console.WriteLine($"Using Roslyn: {expression} = {result}");
-
 
     return 0;
 }
@@ -39,46 +67,66 @@ async Task<double> CalcUsingRoslyn(string expression)
     }
 }
 
-double Calc2(string expression)
+double Calc(string expression)
 {
     var i = 0;
-    var result = Calc(expression, ref i);
+    var result = Evaluate(expression, ref i);
     return result;
 }
 
-
-double Calc(string expression, ref int i)
+double Evaluate(string expression, ref int i)
 {
     var result = 0d;
     while (expression.Length > i)
     {
-        switch (expression[i])
+        i++;
+        switch (expression[i - 1])
         {
             case '(':
-                i++;
-                result = Calc(expression, ref i);
+                result = Evaluate(expression, ref i);
                 break;
+            case ')':
+                return result;
             case > '0' and < '9' or '.':
-                return GetNumber(expression, ref i);
-            case '+':
-                i++;
-                result += Calc(expression, ref i);
-                break;
-            case '-':
-                i++;
-                result -= Calc(expression, ref i);
+                result = GetNumber(expression, ref i);
                 break;
             case '*':
-                i++;
-                result *= Calc(expression, ref i);
+                result *= Calculate(result, expression, ref i);
                 break;
             case '/':
-                i++;
-                result /= Calc(expression, ref i);
+                result /= Calculate(result, expression, ref i);
                 break;
-            default:
-                i++;
+            case '+':
+                result += Evaluate(expression, ref i);
                 break;
+            case '-':
+                result -= Evaluate(expression, ref i);
+                break;
+        }
+    }
+
+    return result;
+}
+
+double Calculate(double result, string expression, ref int i)
+{
+    while (expression.Length > i)
+    {
+        i++;
+        switch (expression[i - 1])
+        {
+            case '(':
+                return Evaluate(expression, ref i);
+            case > '0' and < '9' or '.':
+                return GetNumber(expression, ref i);
+            case '*':
+                return result * Evaluate(expression, ref i);
+            case '/':
+                return result / Evaluate(expression, ref i);
+            case '+':
+                return result + Evaluate(expression, ref i);
+            case '-':
+                return result - Evaluate(expression, ref i);
         }
     }
 
@@ -87,21 +135,19 @@ double Calc(string expression, ref int i)
 
 double GetNumber(string expression, ref int i)
 {
-    var chars = new List<char> { expression[i] };
-    i++;
+    var startIndex = i - 1;
     while (expression.Length > i)
     {
         if (expression[i] is > '0' and < '9' or '.')
         {
-            chars.Add(expression[i]);
+            i++;
         }
         else
         {
             break;
         }
-
-        i++;
     }
 
-    return Convert.ToDouble(string.Concat(chars), CultureInfo.InvariantCulture);
+    var number = Convert.ToDouble(expression[startIndex..i], CultureInfo.InvariantCulture);
+    return number;
 }
