b152dbd3c5d9d05c4bada1e7bb98129576eac8ea
Anton Antonov <menantonov@gmail.com>
2024-06-05 12:26:58 +0500

    Support of Inverse Trigonometric Functions are added (#11)
    Support of Inverse Trigonometric Functions (both a... or arc...
    notations) are added

--- MathEvaluation/MathFn.cs
+++ MathEvaluation/MathFn.cs
@@ -136,6 +136,68 @@ internal static class MathFn
 
     #endregion
 
+    #region Inverse Trigonometric Functions
+
+    public static double Arcsin(double d)
+    {
+        return Math.Asin(d);
+    }
+
+
+    public static double Arccos(double d)
+    {
+        return Math.Acos(d);
+    }
+
+
+    public static double Arctan(double d)
+    {
+        return Math.Atan(d);
+    }
+
+    /// <summary>
+    ///     Inverse Cosecant
+    /// </summary>
+    /// <param name="d"></param>
+    /// <returns></returns>
+    public static double Arccsc(double d)
+    {
+        return d == 0d ? double.NaN : Math.Asin(1 / d);
+    }
+
+    /// <summary>
+    ///     Inverse Secant
+    /// </summary>
+    /// <param name="d"></param>
+    /// <returns></returns>
+    public static double Arcsec(double d)
+    {
+        return d == 0d ? double.NaN : Math.Acos(1 / d);
+    }
+
+    /// <summary>Returns the angle whose cotangent is the specified number.</summary>
+    /// <param name="d">A number representing a cotangent.</param>
+    /// <returns>
+    ///     An angle, θ, measured in radians, such that 0 ≤ θ ≤ π.
+    ///     -or-
+    ///     <see cref="F:System.Double.NaN" /> if <paramref name="d" /> equals <see cref="F:System.Double.NaN" />, 0 if
+    ///     <paramref name="d" /> equals <see cref="F:System.Double.NegativeInfinity" />, or π if <paramref name="d" /> equals
+    ///     <see cref="F:System.Double.PositiveInfinity" />.
+    /// </returns>
+    public static double Arccot(double d)
+    {
+        if (d == 0d)
+            return Math.PI / 2;
+
+        if (double.IsNegative(d))
+            //the Trigonometric Symmetry is applied: arccot(−x) = π − arccot(x)
+            return Math.PI - Math.Atan(1 / -d);
+
+        return Math.Atan(1 / d);
+    }
+
+    #endregion
+
     public static double DegreesToRadians(double degrees)
     {
         return degrees * Math.PI / 180.0d;
@@ -144,8 +206,18 @@ internal static class MathFn
     internal static bool TryGetTrigonometricFn(ReadOnlySpan<char> expression, ref int i,
         out Func<double, double>? fn)
     {
-        if (expression.Length > i + 3 && expression[i + 3] is 'h' or 'H')
-            return TryGetHyperbolicFn(expression, ref i, out fn);
+        if (expression.Length > i + 5 && expression[i] is 'a' or 'A' &&
+            expression[i + 1] is 'r' or 'R' && expression[i + 2] is 'c' or 'C')
+            return TryGetInverseTrigonometricFn(expression, ref i, out fn, 3);
+
+        if (expression.Length > i + 3)
+        {
+            if (expression[i] is 'a' or 'A')
+                return TryGetInverseTrigonometricFn(expression, ref i, out fn, 1);
+
+            if (expression[i + 3] is 'h' or 'H')
+                return TryGetHyperbolicFn(expression, ref i, out fn);
+        }
 
         fn = null;
         if (expression.Length <= i + 2)
@@ -214,6 +286,42 @@ internal static class MathFn
             return false;
 
         i += 4;
+        if (expression.Length > i && expression[i] == '(')
+            i++;
+
+        return true;
+    }
+
+    private static bool TryGetInverseTrigonometricFn(ReadOnlySpan<char> expression, ref int i,
+        out Func<double, double>? fn, int indexShift)
+    {
+        fn = expression[i + indexShift] switch
+        {
+            's' or 'S' => expression[i + indexShift + 1] switch
+            {
+                'i' or 'I' when expression[i + indexShift + 2] is 'n' or 'N' => Arcsin,
+                'e' or 'E' when expression[i + indexShift + 2] is 'c' or 'C' => Arcsec,
+                _ => null
+            },
+            'c' or 'C' => expression[i + indexShift + 1] switch
+            {
+                'o' or 'O' when expression[i + indexShift + 2] is 's' or 'S' => Arccos,
+                'o' or 'O' when expression[i + indexShift + 2] is 't' or 'T' => Arccot,
+                's' or 'S' when expression[i + indexShift + 2] is 'c' or 'C' => Arccsc,
+                _ => null
+            },
+            't' or 'T' => expression[i + indexShift + 1] switch
+            {
+                'a' or 'A' when expression[i + indexShift + 2] is 'n' or 'N' => Arctan,
+                _ => null
+            },
+            _ => null
+        };
+
+        if (fn == null)
+            return false;
+
+        i = i + 3 + indexShift;
         if (expression.Length > i && expression[i] == '(')
             i++;
 
