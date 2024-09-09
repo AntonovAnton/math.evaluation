3067ce9a0d65042cdb0cdfd4159466a1b6236519
Anton Antonov <mail@antonovanton.ru>
2024-09-10 00:34:08 +0500

    Compounding Interest Formula benchmarks.

--- MathEvaluation.Benchmarks/Program.cs
+++ MathEvaluation.Benchmarks/Program.cs
@@ -5,3 +5,4 @@ Console.OutputEncoding = Encoding.UTF8;
 
 BenchmarkRunner.Run<EvaluationBenchmarks>();
 BenchmarkRunner.Run<CompilationBenchmarks>();
+BenchmarkRunner.Run<CompoundingInterestBenchmarks>();
